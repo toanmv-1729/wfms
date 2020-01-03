840c463920086a42539e7eee6d7969c301755b48
maivantoan0808 <toan.mv0823@gmail.com>
2020-01-03 15:36:35 +0700

    Task#53: Push Notify when create update ticket

--- app/Observers/TicketObserver.php
+++ app/Observers/TicketObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\Models\Ticket;
 use App\Traits\TracksHistoryTrait;
+use App\Notifications\NewTicketNotification;
+use App\Notifications\UpdatedTicketNotification;
 
 class TicketObserver
 {
@@ -15,8 +17,24 @@ class TicketObserver
      * @param  \App\Models\Ticket  $role
      * @return void
      */
+    public function created(Ticket $ticket)
+    {
+        $user = $ticket->assignee;
+        $ticketType = config('ticket.tracker')[$ticket->tracker]['name'];
+        $user->notify(new NewTicketNotification($ticket->id, $ticketType, $ticket->title));
+    }
+
+    /**
+     * Handle the role "updated" event.
+     *
+     * @param  \App\Models\Ticket  $role
+     * @return void
+     */
     public function updated(Ticket $ticket)
     {
         $this->track($ticket);
+        $user = $ticket->assignee;
+        $ticketType = config('ticket.tracker')[$ticket->tracker]['name'];
+        $user->notify(new UpdatedTicketNotification($ticket->id, $ticketType, $ticket->title));
     }
 }
